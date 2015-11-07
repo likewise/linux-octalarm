2ec3d5f7d43a879f43fa662269c49e16bf527a88
Anna Horstmann <ahorstmann@phytec.com>
2015-11-06 17:29:22 -0800

    tools: phytec: create script to automate compiling Linux for Jenkins builds
    Configured to build zImage and imx7-phyboard-rdk.dtb with
    config file imx_v7_defconfig
    
    Signed-off-by: Anna Horstmann <ahorstmann@phytec.com>
    (cherry picked from commit cb92f1684500be0eaf9745647955917673b1ae0b)
    (cherry picked from commit 688024513e425ec23c43f9fb5bdf8e1e35809ab9)

---
+++ tools/phytec/build_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd $WORKSPACE
+source /opt/fsl-imx-release/1.8/environment-setup-cortexa7hf-vfp-neon-poky-linux-gnueabi
+make distclean
+make imx_v7_defconfig
+make zImage
+make imx7-phyboard-rdk.dtb
+mkdir images
+rm -rf images/*
+cd arch/arm/boot/
+tar -cvjf "${WORKSPACE}/images/linux-b${BUILD_ID}.tar.bz2" zImage dts/imx7-phyboard-rdk.dtb
