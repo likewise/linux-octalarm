16b4e370400c6269c7ea4ac4c9e3409480703560
Anna Horstmann <ahorstmann@phytec.com>
2016-08-09 16:42:14 -0700

    tools: phytec: update build script to build imx7d-phyboard-zeta.dtb
    Signed-off-by: Anna Horstmann <ahorstmann@phytec.com>
    (cherry picked from commit b6e2a2ecb0870be3be487a8d5a05ef2307b18bfc)
    (cherry picked from commit d1cfe220d2d5066d30cf9e446cb90a3e5e52e96b)

--- tools/phytec/build_script.sh
+++ tools/phytec/build_script.sh
@@ -5,8 +5,8 @@ source /opt/fsl-imx-release/1.8/environment-setup-cortexa7hf-vfp-neon-poky-linux
 make distclean
 make imx7_phyboard_zeta_defconfig
 make zImage
-make imx7-phyboard-zeta.dtb
+make imx7d-phyboard-zeta.dtb
 mkdir images
 rm -rf images/*
 cd arch/arm/boot/
-tar -cvjf "${WORKSPACE}/images/linux-b${BUILD_ID}.tar.bz2" zImage dts/imx7-phyboard-zeta.dtb
+tar -cvjf "${WORKSPACE}/images/linux-b${BUILD_ID}.tar.bz2" zImage dts/imx7d-phyboard-zeta.dtb
