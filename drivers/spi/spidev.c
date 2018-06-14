47a54f339e091fcbefca89f2565c735cfc37ccad
Walter Goossens <waltergoossens@creative-embedded.com>
2018-06-14 12:40:34 +0200

    spidev: Use correct compatible for ws2812b spidev
    Signed-off-by: Walter Goossens <waltergoossens@creative-embedded.com>

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -696,6 +696,9 @@ static struct class *spidev_class;
 static const struct of_device_id spidev_dt_ids[] = {
 	{ .compatible = "rohm,dh2228fv" },
 	{ .compatible = "lineartechnology,ltc2488" },
+	{ .compatible = "ge,achc" },
+	{ .compatible = "semtech,sx1301" },
+	{ .compatible = "ws2812b" },
 	{},
 };
 MODULE_DEVICE_TABLE(of, spidev_dt_ids);
